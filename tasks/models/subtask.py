57c43fe9f093d2beb3c2b92f215d09c4a2dd1fcb
tsurkanOdessa <set.tsurkan@gmail.com>
2025-05-03 11:50:50 +0200

    init models

---
+++ tasks/models/subtask.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .task import Task
+
+class SubTask(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+    title = models.CharField(max_length=255)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.task.title} → {self.title}"
